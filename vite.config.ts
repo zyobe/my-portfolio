c35ca6ee9af5884d1e0fc35d91d2dd17e5393799
Adam Hutson <adamhutson@Adams-MacBook-Pro.local>
2024-05-13 11:20:52 +0100

    Updated based in config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
+  base: '',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
